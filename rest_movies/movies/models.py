922ab4ed5789a21be6f0a53b2694e51dd12ea5b1
Adam Stachura <behir92@gmail.com>
2017-04-03 13:00:35 +0200

    metoda str dodana do modeli

--- rest_movies/movies/models.py
+++ rest_movies/movies/models.py
@@ -2,3 +2,28 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    director = models.ForeignKey(Person, related_name='director')
+    actors = models.ManyToManyField(Person, through='Role')
+    year = models.IntegerField()
+
+    def __str__(self):
+        return "{} ({})".format(self.title,self.year)
+
+
+class Role(models.Model):
+    actor = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='actor')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    role = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "{} as {}".format(self.actor, self.role)
+
+
