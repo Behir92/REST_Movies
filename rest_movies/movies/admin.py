532d427d0bacf4f3987c4d433417cd6b3bf316f8
Adam Stachura <behir92@gmail.com>
2017-04-03 13:00:10 +0200

    modele dodane do panelu

--- rest_movies/movies/admin.py
+++ rest_movies/movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from movies.models import Movie, Person, Role
 
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Movie)
+admin.site.register(Role)
